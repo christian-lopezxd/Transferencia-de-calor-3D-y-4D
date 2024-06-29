fc58e679ccabeb2a5c9539f5a82138d12afeb274
christian-lopezxd <00179320@uca.edu.sv>
2024-06-28 22:13:08 -0600

    Comments for mef_proccess

--- Parte 2 - Proceso/mef_utilities/mef_process.hpp
+++ Parte 2 - Proceso/mef_utilities/mef_process.hpp
@@ -1,3 +1,7 @@
+// Esta función calcula el volumen local de un elemento tetraédrico.
+// Calcula la matriz V que contiene las diferencias de coordenadas entre los nodos.
+// Calcula el determinante de V y lo divide entre 6 para obtener el volumen.
+
 float calculate_local_volume(
     float x1, float y1, float z1,
     float x2, float y2, float z2,
@@ -11,6 +15,8 @@ float calculate_local_volume(
     return abs(determinant(&V))/6;
 }
 
+// Esta función calcula el jacobiano local del elemento tetraédrico.
+
 float calculate_local_jacobian(
     float x1, float y1, float z1,
     float x2, float y2, float z2,
@@ -20,11 +26,17 @@ float calculate_local_jacobian(
     return (x2-x1)*(y3-y1)*(z4-z1)-(x2-x1)*(y4-y1)*(z3-z1)-(x3-x1)*(y2-y1)*(z4-z1)+(x3-x1)*(y4-y1)*(z2-z1)+(x4-x1)*(y2-y1)*(z3-z1)-(x4-x1)*(y3-y1)*(z2-z1);
 }
 
+
+// Esta función calcula la matriz B 
+
 void calculate_B(Matrix* B){
     B->set(-1, 0, 0); B->set(1, 0, 1); B->set(0, 0, 2); B->set(0, 0, 3);
     B->set(-1, 1, 0); B->set(0, 1, 1); B->set(1, 1, 2); B->set(0, 1, 3);
     B->set(-1, 2, 0); B->set(0, 2, 1); B->set(0, 2, 2); B->set(1, 2, 3); 
 }
+
+// Calcula la matriz A
+// Recibe las coordenadas de los 4 nodos del elemento.
 
 void calculate_local_A(Matrix* A, 
     float x1, float y1, float z1,
@@ -39,12 +51,16 @@ void calculate_local_A(Matrix* A,
 
 void create_local_K(Matrix* K, long element_id, Mesh* M){
     K->set_size(4,4);
+// Obtiene la conductividad térmica del problema desde el objeto Mesh
 
     float k = M->get_problem_data(THERMAL_CONDUCTIVITY);
+    // Obtiene las coordenadas de los nodos del elemento
+
     float x1 = M->get_element(element_id)->get_node1()->get_x_coordinate(), y1 = M->get_element(element_id)->get_node1()->get_y_coordinate(), z1 = M->get_element(element_id)->get_node1()->get_z_coordinate(),
           x2 = M->get_element(element_id)->get_node2()->get_x_coordinate(), y2 = M->get_element(element_id)->get_node2()->get_y_coordinate(), z2 = M->get_element(element_id)->get_node2()->get_z_coordinate(),
           x3 = M->get_element(element_id)->get_node3()->get_x_coordinate(), y3 = M->get_element(element_id)->get_node3()->get_y_coordinate(), z3 = M->get_element(element_id)->get_node3()->get_z_coordinate(),
           x4 = M->get_element(element_id)->get_node4()->get_x_coordinate(), y4 = M->get_element(element_id)->get_node4()->get_y_coordinate(), z4 = M->get_element(element_id)->get_node4()->get_z_coordinate();
+          // Calcula el volumen y el jacobiano local del elemento  
     float Volume = calculate_local_volume(
         x1,y1,z1,
         x2,y2,z2,
@@ -73,6 +89,7 @@ void create_local_K(Matrix* K, long element_id, Mesh* M){
     transpose(&A,3,3,&At);
     //Bt.show(); At.show();
 
+// Declaracion de las matrices auxiliares para el cálculo
     Matrix res1, res2, res3;
     product_matrix_by_matrix(&A,&B,&res1);
     product_matrix_by_matrix(&At,&res1,&res2);
@@ -82,6 +99,7 @@ void create_local_K(Matrix* K, long element_id, Mesh* M){
     //cout << "\t\tLocal matrix created for Element " << element_id+1 << ": "; K->show(); cout << "\n";
 }
 
+// Esta función crea el vector local b
 void create_local_b(Vector* b, long element_id, Mesh* M){
     b->set_size(4);
 
@@ -105,6 +123,7 @@ void create_local_b(Vector* b, long element_id, Mesh* M){
     //cout << "\t\tLocal vector created for Element " << element_id+1 << ": "; b->show(); cout << "\n";
 }
 
+// Esta función crea los sistemas locales
 void create_local_systems(Matrix* Ks, Vector* bs, long num_elements, Mesh* M){
     for(int e = 0; e < num_elements; e++){
         cout << "\tCreating local system for Element " << e+1 << "...\n\n";
@@ -113,6 +132,8 @@ void create_local_systems(Matrix* Ks, Vector* bs, long num_elements, Mesh* M){
     }
 }
 
+
+// Se ensambla la matriz global K
 void assembly_K(Matrix* K, Matrix* local_K, long index1, long index2, int index3, int index4){
     K->add(local_K->get(0,0),index1,index1);    K->add(local_K->get(0,1),index1,index2);    K->add(local_K->get(0,2),index1,index3);    K->add(local_K->get(0,3),index1,index4);
     K->add(local_K->get(1,0),index2,index1);    K->add(local_K->get(1,1),index2,index2);    K->add(local_K->get(1,2),index2,index3);    K->add(local_K->get(1,3),index2,index4);
@@ -120,6 +141,7 @@ void assembly_K(Matrix* K, Matrix* local_K, long index1, long index2, int index3
     K->add(local_K->get(3,0),index4,index1);    K->add(local_K->get(3,1),index4,index2);    K->add(local_K->get(3,2),index4,index3);    K->add(local_K->get(3,3),index4,index4);
 }
 
+// Lo mismo para el vector global b
 void assembly_b(Vector* b, Vector* local_b, long index1, long index2, int index3, int index4){
     b->add(local_b->get(0),index1);
     b->add(local_b->get(1),index2);
@@ -144,6 +166,7 @@ void assembly(Matrix* K, Vector* b, Matrix* Ks, Vector* bs, long num_elements, M
     }
 }
 
+// Aplica las condiciones de Neumann al vector b
 void apply_neumann_boundary_conditions(Vector* b, Mesh* M){
     long num_conditions = M->get_quantity(NUM_NEUMANN);
 
@@ -161,6 +184,7 @@ void add_column_to_RHS(Matrix* K, Vector* b, int col, float T_bar){
         b->add(-T_bar*K->get(r,col),r);
 }
 
+// Aplica las condiciones de Dirichlet a la matriz K y al vector b
 void apply_dirichlet_boundary_conditions(Matrix* K, Vector* b, Mesh* M){
     long num_conditions = M->get_quantity(NUM_DIRICHLET);
     int previous_removed = 0;
